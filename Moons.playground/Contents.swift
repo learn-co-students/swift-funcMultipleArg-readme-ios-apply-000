62f9fd94ca8086e4ea8c10734657fa53fad3199b
shahaalmh201 <shahalamh201@gmail.com>
2016-10-18 21:38:33 -0400

    MultipleArg

--- Moons.playground/Contents.swift
+++ Moons.playground/Contents.swift
@@ -7,7 +7,7 @@ moonsOfJupiter()
 
 // planetDescription, take 1
 // The bit of code below is commented out, since we iterate on this definition of planetDescription() below.
-/*
+
 func planetDescription(planet: String) {
     print("There are 67 moons orbiting \(planet).")
 }
@@ -17,7 +17,7 @@ planetDescription(planet: "Jupiter")
 var planet = "Mars"
 
 planetDescription(planet: planet)
-*/
+
 
 
 func greet(name: String, greeting: String) {
@@ -29,15 +29,39 @@ func planetDescription(planet: String, numberOfMoons: Int) {
     print("There are \(numberOfMoons) moons orbiting \(planet)")
 }
 
+
 // This line of code gives an error.
 // Uncomment it to see what the error is!
-//planetDescription("Jupiter", 67)
+
+planetDescription("Jupiter", 67)
 
 planetDescription(planet: "Jupiter", numberOfMoons: 67)
 
-var planet = "Jupiter"
+var planet = "Jupiter" // This is an error.
 var moons = 67
 planetDescription(planet: planet, numberOfMoons: moons)
 planet = "Mars"
 moons = 2
 planetDescription(planet: planet, numberOfMoons: moons)
+
+
+func theBestFunction(firstParameterName: String, secondParameterName: String) {
+    
+    // function body here
+    
+    // firstParameterName and secondParameterName refer to the argument values for the first and second parameters
+    theBestFunction(firstParameterName: "Brick", secondParameterName: "Oven")
+    
+}
+
+
+func theBestFunction(_ firstParameterName: String, _ secondParameterName: String, _ thirdParameterName: String, _ fourthParameterName: String) {
+    
+    print(firstParameterName)
+    print(secondParameterName)
+    print(thirdParameterName)
+    print(fourthParameterName)
+    
+}
+
+
