76c6ab02fcd744a4bd1a5903684513268b81ad0b
Emily Carlo <EmilyCarlo@172-16-176-194.DYNAPOOL.NYU.EDU>
2016-12-05 18:15:49 -0500

    Complete problems

--- Moons.playground/Contents.swift
+++ Moons.playground/Contents.swift
@@ -5,6 +5,20 @@ func moonsOfJupiter() {
 moonsOfJupiter()
 
 
+// planetDescription, take 1
+// The bit of code below is commented out, since we iterate on this definition of planetDescription() below.
+/*
+func planetDescription(planet: String) {
+    print("There are 67 moons orbiting \(planet).")
+}
+
+planetDescription(planet: "Jupiter")
+
+var planet = "Mars"
+
+planetDescription(planet: planet)
+*/
+
 
 func greet(name: String, greeting: String) {
     print("\(greeting), \(name)!")
@@ -15,6 +29,9 @@ func planetDescription(planet: String, numberOfMoons: Int) {
     print("There are \(numberOfMoons) moons orbiting \(planet)")
 }
 
+// This line of code gives an error.
+// Uncomment it to see what the error is!
+//planetDescription("Jupiter", 67)
 
 planetDescription(planet: "Jupiter", numberOfMoons: 67)
 
@@ -24,10 +41,6 @@ planetDescription(planet: planet, numberOfMoons: moons)
 planet = "Mars"
 moons = 2
 planetDescription(planet: planet, numberOfMoons: moons)
-
-func theBestFunction(firstParameterName: String, secondParameterName: String) {
-    theBestFunction(firstParameterName: "Brick", secondParameterName: "Oven")
-}
 
 func theBestFunction(_ firstParameterName: String, _ secondParameterName: String, _ thirdParameterName: String, _ fourthParameterName: String) {
     
